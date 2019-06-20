1e06a183e9b61409bae72873d9c5fe8ac1b4ee6d
zhangsunny <2292280387@qq.com>
2019-06-20 09:56:53 +0800

    second

--- utils/models.py
+++ utils/models.py
@@ -11,16 +11,51 @@ class FCGenerator(nn.Module):
 
         def block(in_feat, out_feat):
             layers = [
-                nn.Linear(latent_dim, 1024),
-                nn.BatchNorm1d(1024),
-                nn.LeakyReLU(),
+                nn.Linear(in_feat, out_feat),
+                nn.BatchNorm1d(out_feat),
+                nn.LeakyReLU(self.relu_slope),
                 nn.Dropout(self.drop_rate),
             ]
         return layers
         self.fc = nn.Sequential(
-            nn.Linear(latent_dim, 1024),
-            nn.BatchNorm1d(1024),
-            nn.LeakyReLU(),
-            nn.Dropout(self.drop_rate),
-
+            *block(latent_dim, 1024),
+            *block(1024, 512),
+            nn.Linear(512, np.prod(input_shape)),
+            nn.Tanh(),
         )
+
+    def forward(self, z):
+        if z.dim() > 2:
+            z = z.view(z.size(0), -1)
+        out = self.fc(z)
+        out = out.view(out.size(0), *self.input_shape)
+        return out
+
+
+class FCDiscriminator(nn.Module):
+    def __init__(self, input_shape):
+        super(FCDiscriminator, self).__init__()
+        self.input_shape = input_shape
+        self.relu_slope = 0.2
+        self.drop_rate = 0.25
+
+        def block(in_feat, out_feat):
+            layers = [
+                nn.Linear(in_feat, out_feat),
+                nn.BatchNorm1d(out_feat),
+                nn.LeakyReLU(self.relu_slope),
+                nn.Dropout(self.drop_rate),
+            ]
+        return layers
+        self.fc = nn.Sequential(
+            *block(latent_dim, 1024),
+            *block(1024, 512),
+            *block(512, 64),
+            nn.Linear(64, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        out = self.fc(x)
+        return out
